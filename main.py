48e776eafc14c4c39059dda106b163af1ab67b46
Joel Burget <joelburget@gmail.com>
2022-10-20 08:16:15 -0700

    Fix main.py (even though I'm not using it).

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import data
-import train
+from hyperparams import *
+from model import Transformer
 from pathlib import Path
+import train
+
+model = Transformer(
+    num_layers=num_layers,
+    d_vocab=d_vocab,
+    d_model=d_model,
+    d_mlp=d_mlp,
+    d_head=d_head,
+    num_heads=num_heads,
+    n_ctx=n_ctx,
+    act_type=act_type,
+    use_cache=False,
+    use_ln=use_ln,
+)
+model.to("cuda")
 
 if __name__ == "__main__":
-    train.run_training(Path("."), "div", data.div_train, data.div_test)
+    train.run_training(Path("."), "div", data.div_train, data.div_test, model)
